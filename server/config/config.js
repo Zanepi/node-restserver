eac4f59b38b8e5b3d2c1329580b210d19cdbbac2
Zanepi <brian.galdames@gmail.com>
2020-05-05 18:35:22 -0400

    RestServer - usuario - repara ConnStr

--- server/config/config.js
+++ server/config/config.js
@@ -12,6 +12,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //Entorno
 //===============
 let urlDB = process.env.NODE_ENV === 'dev' ?
-    'mongodb://localhost:27017/cafe' : 'mongodb+srv://challenger:u1Mt9cORqKUJ5WJA@zanepidev-a4kdp.azure.mongodb.net/test';
+    'mongodb://localhost:27017/cafe' : 'mongodb+srv://challenger:u1Mt9cORqKUJ5WJA@zanepidev-a4kdp.azure.mongodb.net/cafe';
 
 process.env.URLDB = urlDB;
